10f7b104f5659c6ecf5abc136bc9d88e88fd39df
Leonardo <148665981+LeoCoelhoP@users.noreply.github.com>
2024-09-11 17:44:47 -0300

    Fix: ReferenceError: err is not defined at startServer (/opt/render/project/src/server.js:39:56)

--- server.js
+++ server.js
@@ -9,36 +9,36 @@ const MONGODB_URI = process.env.MONGODB;
 const port = process.env.PORT || 8000;
 
 async function startServer() {
-	try {
-		await mongoose.connect(MONGODB_URI);
-		console.log('DB successfully connected');
+  try {
+    await mongoose.connect(MONGODB_URI);
+    console.log('DB successfully connected');
 
-		const server = http.createServer(app);
-		server.listen(port, () => {
-			console.log(`Server runing on port: ${port}`);
-		});
+    const server = http.createServer(app);
+    server.listen(port, () => {
+      console.log(`Server runing on port: ${port}`);
+    });
 
-		server.on('error', (error) => {
-			if (error.syscall !== 'listen') {
-				throw error;
-			}
+    server.on('error', (error) => {
+      if (error.syscall !== 'listen') {
+        throw error;
+      }
 
-			const bind = typeof port === 'string' ? `Pipe ${port}` : `Port ${port}`;
-			switch (error.code) {
-				case 'EACCES':
-					console.error(`${bind} requires elevated privileges`);
-					process.exit(1);
-				case 'EADDRINUSE':
-					console.error(`${bind} is already in use`);
-					process.exit(1);
-				default:
-					throw error;
-			}
-		});
-	} catch (error) {
-		console.error('Failed to connect start the server.', err);
-		process.exit(1);
-	}
+      const bind = typeof port === 'string' ? `Pipe ${port}` : `Port ${port}`;
+      switch (error.code) {
+        case 'EACCES':
+          console.error(`${bind} requires elevated privileges`);
+          process.exit(1);
+        case 'EADDRINUSE':
+          console.error(`${bind} is already in use`);
+          process.exit(1);
+        default:
+          throw error;
+      }
+    });
+  } catch (error) {
+    console.error('Failed to connect start the server.', error);
+    process.exit(1);
+  }
 }
 
 startServer();
